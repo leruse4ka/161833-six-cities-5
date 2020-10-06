545b4029b983b65c5ab98d0b37efe0268993ee4e
leruse4ka <60311701+leruse4ka@users.noreply.github.com>
2020-10-06 19:31:30 +0500

    Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
 
+const Settings = {
+  COUNT_RENT: 312
+};
+
+ReactDOM.render(
+    <App
+      countRent={Settings.COUNT_RENT}
+    />,
+    document.querySelector(`#root`)
+);
