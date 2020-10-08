59f786267e2950d6daccf5e9be4e8c4aa9003efd
leruse4ka <60311701+leruse4ka@users.noreply.github.com>
2020-10-08 22:05:47 +0500

    Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import AuthScreen from "../auth-screen/auth-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import PropertyScreen from "../property-screen/property-screen";
 
 const App = (props) => {
   const {countRent} = props;
 
   return (
-    <Main countRent={countRent}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main countRent={countRent}/>
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route path="/offer/:id" exact component={PropertyScreen} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
